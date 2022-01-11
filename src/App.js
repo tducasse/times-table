1e2ddf81e724f6aa09aa3c0218ffec6c61724406
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2022-01-11 15:18:47 +1100

    Add other operations

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [step, setStep] = useState(0)
   const [errors, setErrors] = useState([])
+  const [sign, setSign] = useState('multiplication')
 
   const startGame = () => setStep(1)
 
@@ -19,14 +20,14 @@ const App = () => {
   }
 
   return <>
-    {step === 0 && <Setup max={max} number={number} setNumber={setNumber} setMax={setMax} startGame={startGame} time={time} setTime={setTime} />}
-    {step === 1 && <Game max={max} number={number} time={time} stopGame={stopGame} score={score} setScore={setScore} errors={errors} setErrors={setErrors} />}
-    {step === 2 && <Results number={number} score={score} back={back} errors={errors} />}
+    {step === 0 && <Setup max={max} number={number} setNumber={setNumber} setMax={setMax} startGame={startGame} time={time} setTime={setTime} sign={sign} setSign={setSign} />}
+    {step === 1 && <Game max={max} number={number} time={time} stopGame={stopGame} score={score} setScore={setScore} errors={errors} setErrors={setErrors} sign={sign} />}
+    {step === 2 && <Results number={number} score={score} back={back} errors={errors} sign={sign} />}
   </>
 
 }
 
-const Setup = ({ max, number, setMax, setNumber, startGame, setTime, time }) => {
+const Setup = ({ max, number, setMax, setNumber, startGame, setTime, time, sign, setSign }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     startGame()
@@ -43,6 +44,13 @@ const Setup = ({ max, number, setMax, setNumber, startGame, setTime, time }) => 
         <label>Time in seconds</label>
         <input type="number" value={time} onChange={(e) => setTime(e.target.value)} />
 
+        <label>Operation</label>
+        <select type="number" value={sign} onChange={(e) => setSign(e.target.value)} >
+          <option value="multiplication">Multiplication</option>
+          <option value="addition">Addition</option>
+          <option value="subtraction">Subtraction</option>
+        </select>
+
         <button type="submit">Start game</button>
       </center>
     </form>
@@ -51,29 +59,48 @@ const Setup = ({ max, number, setMax, setNumber, startGame, setTime, time }) => 
 
 const getNumber = (max) => Math.round(0.5 + Math.random() * max);
 
-const getOperation = (max) => {
-  const first = getNumber(max);
-  const second = getNumber(10);
-  return {
-    result: String(first * second),
-    first,
-    second,
-    question: `${first} X ${second}`
+const symbols = {
+  addition: "+",
+  subtraction: "-",
+  multiplication: "*"
+}
+
+const getResult = (first, second, sign) => {
+  switch (sign) {
+    case "addition":
+      return first + second;
+    case "subtraction":
+      return first - second;
+    case "multiplication":
+      return first * second;
+    default:
+      return 0;
   }
 }
 
-const getOperations = (max, number) => {
+const getOperation = (max, sign) => {
+  const first = getNumber(max);
+  const second = getNumber(10);
+  return {
+    result: String(getResult(first, second, sign)),
+    first,
+    second,
+    question: `${first} ${symbols[sign]} ${second}`
+  }
+}
+
+const getOperations = (max, number, sign) => {
   const operations = [];
   for (let i = 0; i < number; i++) {
-    operations.push(getOperation(max));
+    operations.push(getOperation(max, sign));
   }
   return operations;
 }
 
 
-const Game = ({ number, max, time, setScore, score, stopGame, errors, setErrors }) => {
+const Game = ({ number, max, time, setScore, score, stopGame, errors, setErrors, sign }) => {
   const [count, setCount] = useState(1);
-  const [operations] = useState(getOperations(max, number));
+  const [operations] = useState(getOperations(max, number, sign));
 
   const onAnswer = (answer) => {
     if (answer === operations[count - 1].result) {
@@ -116,7 +143,7 @@ const Question = ({ question, onAnswer, score, number, time, stopGame }) => {
   </form >
 }
 
-const Results = ({ number, score, errors, back }) => <center>
+const Results = ({ number, score, errors, back, sign }) => <center>
   <h4>Number of questions: {number}</h4>
   <h4>Score: {score}</h4>
   <h4>Errors: {errors.length}</h4>
@@ -126,7 +153,7 @@ const Results = ({ number, score, errors, back }) => <center>
       <h2>Mistakes:</h2>
       {errors.map(({ first, second, question, result }, index) => (
         <div key={question + index}>
-          <Error first={first} second={second} result={result} />
+          <Error first={first} second={second} result={result} sign={sign} />
         </div>
       ))}
     </div>
@@ -134,9 +161,9 @@ const Results = ({ number, score, errors, back }) => <center>
   <button onClick={back}>Back</button>
 </center>
 
-const Error = ({ first, second, result }) => <div style={{ display: "flex", flexDirection: "column", maxWidth: 250 }}>
+const Error = ({ first, second, result, sign }) => <div style={{ display: "flex", flexDirection: "column", maxWidth: 250 }}>
   <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", }}>
-    <h3>{first}</h3><h4>X</h4><h3>{second}</h3><h4>=</h4><h3>{result}</h3>
+    <h3>{first}</h3><h4>{symbols[sign]}</h4><h3>{second}</h3><h4>=</h4><h3>{result}</h3>
   </div>
 </div>
 
